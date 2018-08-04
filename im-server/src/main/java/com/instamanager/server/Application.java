b59ffd3ecf627a70b760f71176f8f5c53ef00653
Taras Slynko <t.slynko@spd-ukraine.com>
2018-08-04 15:49:34 +0300

    turn off warn WARNING: An illegal reflective access operation has occurred

--- im-server/src/main/java/com/instamanager/server/Application.java
+++ im-server/src/main/java/com/instamanager/server/Application.java
@@ -1,5 +1,6 @@
 package com.instamanager.server;
 
+import com.instamanager.server.util.WarnUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
+        WarnUtil.disableWarning();
         SpringApplication.run(Application.class, args);
     }
 
---
+++ im-server/src/main/java/com/instamanager/server/util/WarnUtil.java
@@ -0,0 +1,23 @@
+package com.instamanager.server.util;
+
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
+
+public class WarnUtil {
+
+    public static void disableWarning() {
+        try {
+            Field theUnsafe = Unsafe.class.getDeclaredField("theUnsafe");
+            theUnsafe.setAccessible(true);
+            Unsafe u = (Unsafe) theUnsafe.get(null);
+
+            Class cls = Class.forName("jdk.internal.module.IllegalAccessLogger");
+            Field logger = cls.getDeclaredField("logger");
+            u.putObjectVolatile(cls, u.staticFieldOffset(logger), null);
+        } catch (Exception e) {
+            // ignore
+        }
+    }
+
+}
